917a288dd05b457f22ff50e706f4d95995c3dba1
Sahasra B <72568874+sahasraBalguri@users.noreply.github.com>
2025-03-23 23:36:28 -0400

    Update authController.js
    checking user if already exists/no

--- grok-chat-assistant/server/grok-chat-assistant/server/Contollers/authController.js
+++ grok-chat-assistant/server/grok-chat-assistant/server/Contollers/authController.js
@@ -5,4 +5,7 @@ exports.register = async (req, res) => {
 try{
   const{username, email, password}= req.body;
 
-  let user
+  let user = await User.findOne({ $or: [{ email }, { username }] });
+   if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
