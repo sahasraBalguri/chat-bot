9c36687335abb559bfd270946fa80f0557dec047
Sahasra B <72568874+sahasraBalguri@users.noreply.github.com>
2025-03-30 23:51:11 -0400

    Update index.js

--- grok-chat-assistant/server/grok-chat-assistant/server/index.js
+++ grok-chat-assistant/server/grok-chat-assistant/server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
+  .then(() => console.log('Connected to MongoDB')) 
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
